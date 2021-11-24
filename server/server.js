7f3bdb2fb94814e29b03d2e8ac2c5ab6cff44483
Julian Kuyumcu <julesjk23@hotmail.com>
2021-11-23 21:20:48 -0500

    Heroku integration

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
+app.use(cors({origin: ["http://localhost:3000"]}));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
