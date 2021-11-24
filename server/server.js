275973bad880ead0019606cf27ab1c259a1005b4
Julian Kuyumcu <julesjk23@hotmail.com>
2021-11-24 00:30:16 -0500

    Heroku integration

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "client/build", "index.html"));
 });
 
 app.post('/api/emailCheck', (req, res) => {
