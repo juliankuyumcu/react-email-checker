dd0bca8943b6e34dcd513822dcdbb28d0293a574
Julian Kuyumcu <julesjk23@hotmail.com>
2021-11-23 22:03:26 -0500

    Heroku integration

--- server/server.js
+++ server/server.js
@@ -9,13 +9,6 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.post('/api/test', (req, res) => {
-    if(req.body.firstName.includes("<"))
-        res.status(400).json("Bad request!")
-
-    res.json("Your name is: " + req.body.firstName + " " + req.body.lastName);
-});
-
 app.post('/api/emailCheck', (req, res) => {
     if(/^[\w\.]+\@[^ \@]+\.[A-z]{2,4}$/.test(req.body.email)) {
         res.json("valid");
